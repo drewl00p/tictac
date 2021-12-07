83d87fad6bea9750896d495a9e9e9a2b0b4e79d6
ordrew <55066792+ordrew@users.noreply.github.com>
2021-12-07 11:57:35 -0500

    fixed game logic?

--- js/Game.js
+++ js/Game.js
@@ -1,24 +1,55 @@
 export default class Game {
 	constructor() {
 		this.turn = "X";
+		this.lastMove = null;
 		this.board = new Array(9).fill(null);
+		this.mainBoard = new Array(9).fill(this.board);
+		this.validMoves = new Array(9).fill(true); 
+		this.winBoard = new Array(9).fill(null);
+	}
+
+	setValidLocations() {
+		for (index in this.validMoves) {
+			if (!this.mainBoard[index].includes(null)) {
+				if (this.lastMove == index) {
+					this.validMoves[index] = true;
+				}
+			} else {
+				this.validMoves[index] = true;
+			}
+		}
 	}
 
 	nextTurn() {
-		this.turn = this.turn === "X" ? "O" : "X";
+		this.turn = this.turn === "X" ? "O" : "X";		
 	}
+
+
 
 	//Makes a move
 	makeMove(i) {
+
+		//check if board is in full/won
 		if (!this.isInProgress()) {
 			return;
 		}
 
+		//check if space is filled
 		if (this.board[i]) {
 			return;
 		}
 
+		//update valid locations
+		this.setValidLocations();
+
+		//check if move is allowed relative to opponent's last move
+		if (this.validMoves[i] === false) {
+			return;
+		}
+
 		this.board[i] = this.turn;
+
+		this.turn = last.turn;
 
 		if (!this.checkWin()) {
 			this.nextTurn();
@@ -26,7 +57,7 @@ export default class Game {
 	}
 
 	//Checks if current board contains winning combination
-	checkWin() {
+	checkWin(boardType) {
 		const winCombos = [
 			[0, 1, 2],
 			[3, 4, 5],
@@ -40,8 +71,7 @@ export default class Game {
 
 		for (const combination of winCombos) {
 			const [a, b, c] = combination;
-
-			if (this.board[a] && (this.board[a] === this.board[b] && this.board[a] === this.board[c])) {
+			if (boardType[a] && (boardType[a] === boardType[b] && boardType[a] === this.boardType[c])) {
 				return combination;
 			}
 		}
@@ -49,7 +79,7 @@ export default class Game {
 	}
 
 	//Checks if game has been won or if game is a draw
-	isInProgress() {
-		return !this.checkWin() && this.board.includes(null);
+	isInProgress(boardType) {
+		return !this.checkWin(boardType) && this.board.includes(null);
 	}
 }
--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ import GameView from "./GameView.js";
 let game = new Game();
 let gameView = new GameView(document.getElementById("app"));
 
+console.log(game.validMoves)
+
 gameView.onTileClick = function (i) {
 	game.makeMove(i);
 	gameView.update(game);
